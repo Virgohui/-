27c9900de100ea8e8861b7cdf7453adf1e5a40d0
yiwuyouyou <yiwuyouyou@bogon.(none)>
2012-10-30 09:35:01 +0800

    Initial Commit

---
+++ yyy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yyy
+//
+//  Created by yiwuyouyou  on 12-10-30.
+//  Copyright (c) 2012年 yiwuyouyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
